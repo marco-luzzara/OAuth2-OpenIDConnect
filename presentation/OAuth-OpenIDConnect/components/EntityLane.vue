9a65fce667f7eafe38581790f18fd83be700715d
Marco Luzzara <marco.luzzara@hotmail.it>
2023-04-18 23:50:27 +0200

    start presentation and components building

---
+++ presentation/OAuth-OpenIDConnect/components/EntityLane.vue
@@ -0,0 +1,12 @@
+<script lang="ts">
+export default {
+    props: ['title']
+}
+</script>
+
+<template>
+    <div style="height: 100%; display: flex; flex-direction: column; align-items: center;">
+        <h4>{{ title }}</h4>
+        <hr style="width: 2vm; height: 100%; background-color: white;" />
+    </div>
+</template>
