9d1d1ede872cb7fde79e478175e6c60d7dabb4f9
Marco Luzzara <marco.luzzara@hotmail.it>
2023-04-29 17:26:10 +0200

    add Slide for code flow starting

---
+++ presentation/OAuth-OpenIDConnect/components/Message.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+export default {
+    props: ['direction']
+}
+</script>
+
+<template>
+    <div style="display: flex; flex-direction: row; align-items: center;">
+        <ic-baseline-keyboard-arrow-left v-if="direction === 'left'" class="text-5xl" />
+        <slot />
+        <ic-baseline-keyboard-arrow-right v-if="direction === 'right'" class="text-5xl" />
+    </div>
+</template>
